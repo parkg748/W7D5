04114371f50fe2564ecb42224161e1c9f38b7d58
Grace <parkg748@newschool.edu>
2018-10-06 02:39:06 -0700

    Finish W7D5

---
+++ BenchBnB/BenchBnB/frontend/components/login_form_container.js
@@ -0,0 +1,14 @@
+import {connect} from 'react-redux';
+import SessionForm from './session_form';
+import {login} from '../../actions/session_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    errors: state.errors,
+    formType: 'login'
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  processForm: (currentUser) => dispatch(login(currentUser));
+};
